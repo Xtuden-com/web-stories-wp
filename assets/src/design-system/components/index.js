80b50c8714dfffdcbb2b2ab212447f03929acbff
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-11-06 16:16:14 -0700

    swap delete publisher logo dialog

--- assets/src/design-system/components/index.js
+++ assets/src/design-system/components/index.js
@@ -18,3 +18,4 @@ export { Text, Display, Headline } from './typography';
 export * from './button';
 export { Modal } from './modal';
 export { Pill } from './pill';
+export { Dialog } from './dialog';
